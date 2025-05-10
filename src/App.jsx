606920c96a248ae3fd7138d0e95fae35e6893a67
kbrown <kamauri205@gmail.com>
2025-05-10 12:08:08 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+
+// createBrowserRouter();
+export default function App() {
+  return <div>Hello Vite!</div>;
+}
